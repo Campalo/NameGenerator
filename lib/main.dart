a4e0ab1344fb2c9634bc020b4f95c6d687e69426
Campalo <25295489+Campalo@users.noreply.github.com>
2019-05-26 16:56:01 +0200

    add favorite functionality

--- lib/main.dart
+++ lib/main.dart
@@ -28,18 +28,53 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(
-          'Awesome Names Generator',
-          style: TextStyle(fontSize: 28)
-        ),
+        title: Text('Awesome Names Generator', style: TextStyle(fontSize: 28)),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved
+          ),
+        ],
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                 title: Text(
+                  pair.asPascalCase,
+                  style: TextStyle(fontSize: 18, color: Colors.pink[600]),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+            return Scaffold(
+              appBar:AppBar(title: Text('Favorite names', style: TextStyle(fontSize: 28))
+              ),
+              body: ListView(children: divided),
+            );
+        },
+      ),
     );
   }
 
@@ -55,10 +90,12 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(int index) {
+    final WordPair pair = _suggestions[index];
     final Color boxColor = index.isOdd ? Colors.brown[50] : Colors.pink[50];
     final Color textColor = index.isOdd
         ? Theme.of(context).primaryColor
         : Theme.of(context).accentColor;
+    final bool alreadySaved = _saved.contains(pair);
 
     return Container(
         padding: EdgeInsets.all(8),
@@ -74,11 +111,23 @@ class RandomWordsState extends State<RandomWords> {
           ],
         ),
         child: ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: TextStyle(fontSize: 18, color: textColor),
-            textAlign: TextAlign.center,
-          ),
-        ));
+            title: Text(
+              pair.asPascalCase,
+              style: TextStyle(fontSize: 18, color: textColor),
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.pink : null,
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(pair);
+                } else {
+                  _saved.add(pair);
+                }
+              });
+            }));
   }
 }
